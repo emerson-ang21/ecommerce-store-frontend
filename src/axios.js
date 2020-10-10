01bbfe66da74c6329bfc17be156cf97de901b225
Javie Onglao <jonglao@rivian.com>
2020-10-10 16:26:35 -0700

    Fix

--- src/axios.js
+++ src/axios.js
@@ -4,5 +4,7 @@ const Axios = axios.create({
   baseURL: process.env.BASE_URL || 'https://localhost'
 });
 
+console.log(`Using URL ${process.env.BASE_URL || 'https://localhost'}. Env is ${process.env.BASE_URL}`)
+
 export default Axios;
 
