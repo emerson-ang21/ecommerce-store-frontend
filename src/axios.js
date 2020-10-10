8243d58bf637f1d91bc92042af7ff558781bdcb3
Javie Onglao <jonglao@rivian.com>
2020-10-10 16:34:05 -0700

    Fix

--- src/axios.js
+++ src/axios.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const Axios = axios.create({
-  baseURL: process.env.BASE_URL || 'https://localhost'
+  baseURL: process.env.BASE_URL || 'https://phfarms.herokuapp.com/'
 });
 
-console.log(`Using URL ${process.env.BASE_URL || 'https://localhost'}. Env is ${process.env.BASE_URL}`)
+console.log(`Using URL ${process.env.BASE_URL || 'https://phfarms.herokuapp.com/'}. Env is ${process.env.BASE_URL}`)
 
 
 export default Axios;
