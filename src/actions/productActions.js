ad6ffc224d442e3f97c2566a48716d09ba2e9d99
Javie Onglao <jonglao@rivian.com>
2020-10-10 17:22:10 -0700

    Fixes when products is empty

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -8,8 +8,8 @@ const listProducts = (category = '', searchKeyword = '', sortOrder = '') => asyn
     try {
 
       dispatch({ type: PRODUCT_LIST_REQUEST });
-      const { data } = await Axios.get("/api/products?category=" + category +	
-      "&searchKeyword=" + searchKeyword + "&sortOrder=" + sortOrder);;
+      console.log(`"/api/products?category=" + ${category} +	&searchKeyword=" + ${searchKeyword} + "&sortOrder=" + ${sortOrder}`);
+      const { data } = await Axios.get("/api/products?category=" + category +	"&searchKeyword=" + searchKeyword + "&sortOrder=" + sortOrder);
       dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
     }
     catch (error) {
