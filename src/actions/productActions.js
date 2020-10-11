7e172bf8de2a232399fbc0da62114c115b8fcea6
Javie Onglao <jonglao@rivian.com>
2020-10-10 17:51:53 -0700

    Fixes when products is empty

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -51,7 +51,7 @@ const saveProduct = (product) => async (dispatch, getState) => {
 const detailsProduct = (productId) => async (dispatch) => {
   try {
     dispatch({ type: PRODUCT_DETAILS_REQUEST, payload: productId });
-    const { data } = await Axios.get("/api/products/" + productId);
+    const { data } = await axios.get("/api/products/" + productId);
     dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: data });
   } catch (error) {
     dispatch({ type: PRODUCT_DETAILS_FAIL, payload: error.message });
